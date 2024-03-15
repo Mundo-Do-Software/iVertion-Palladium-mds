10fd15ca51dfd12e9107d123bc0abb9c97f78b6d
eduardorspada <dudryspada@gmail.com>
2024-03-15 11:14:39 -0300

    Added start date property.

--- API/iVertion.Domain/Entities/TemporaryUserRole.cs
+++ API/iVertion.Domain/Entities/TemporaryUserRole.cs
@@ -7,25 +7,31 @@ namespace iVertion.Domain.Entities
     {
         public string? Role { get; private set; }
         public string? TargetUserId { get; private set; }
+        public DateTime? StartDate { get; private set; }
         public DateTime? ExpirationDate { get; private set; }
+
 
         private void ValidationDomain(string role,
                                       string targetUserId,
-                                      DateTime? expirationDate)
+                                      DateTime startDate,
+                                      DateTime expirationDate)
         {
-            DomainExceptionValidation.When(string.IsNullOrEmpty(role),
+            DomainExceptionValidation.When(String.IsNullOrEmpty(role),
                                            "Invalid Role, must not be null or empty.");
             DomainExceptionValidation.When(role.Length < 5,
                                            "Invalid Role, too short, must be at least 5 characters long.");
             DomainExceptionValidation.When(role.Length > 25,
                                            "Invalid Role, too long, max 25 characters.");
-            DomainExceptionValidation.When(string.IsNullOrEmpty(targetUserId),
+            DomainExceptionValidation.When(String.IsNullOrEmpty(targetUserId),
                                            "Invalid Target User Id, must not be null or empty.");
-            DomainExceptionValidation.When(expirationDate > DateTime.Now,
-                                           "Invalid Expiration Date, must be greater than the current date.");
+            DomainExceptionValidation.When(startDate >= DateTime.Now,
+                                           "Invalid Start Date, must be greater than the current date.");
+            DomainExceptionValidation.When(expirationDate > startDate,
+                                           "Invalid Expiration Date, must be greater than the Start Date.");
 
             Role = role;
             TargetUserId = targetUserId;
+            StartDate = startDate;
             ExpirationDate = expirationDate;
             
         }
