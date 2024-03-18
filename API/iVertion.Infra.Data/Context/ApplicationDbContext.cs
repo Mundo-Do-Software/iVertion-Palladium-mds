95e12bc3a6c6de8393a4553e0d03d0f945f351b7
eduardorspada <dudryspada@gmail.com>
2024-03-18 20:43:07 -0300

    We add the TemporaryUserRole domain to the application context.

--- API/iVertion.Infra.Data/Context/ApplicationDbContext.cs
+++ API/iVertion.Infra.Data/Context/ApplicationDbContext.cs
@@ -12,6 +12,7 @@ namespace iVertion.Infra.Data.Context
         public DbSet<UserProfile>? UserProfiles { get; set; }
         public DbSet<RoleProfile>? RoleProfiles { get; set; }
         public DbSet<AdditionalUserRole>? AdditionalUserRoles { get; set; }
+        public DbSet<TemporaryUserRole>? TemporaryUserRoles { get; set; }
         // Blogs
         public DbSet<Category>? Categories { get; set; }
         public DbSet<Article>? Articles { get; set; }
