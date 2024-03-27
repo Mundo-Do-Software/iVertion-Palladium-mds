247dcfdee149a4552dcdedf568ef66c9c0648bc1
eduardorspada <dudryspada@gmail.com>
2024-03-27 16:04:27 -0300

    Added template for updating a temporary user role.

---
+++ API/iVertion.WebApi/Models/EditTemporaryUserRoleModel.cs
@@ -0,0 +1,34 @@
+
+namespace iVertion.WebApi.Models
+{
+    /// <summary>
+    /// This is the model for update a temporary user role.
+    /// </summary>
+    public class EditTemporaryUserRoleModel
+    {
+        /// <summary>
+        /// An integer as Id.
+        /// </summary>
+        /// <value></value>
+        public int Id { get; set; }
+        /// <summary>
+        /// A role registered in the Identity roles.
+        /// </summary>
+        public string? Role { get; set; }
+
+        /// <summary>
+        /// An existing username on Identity.
+        /// </summary>
+        public string? UserName { get; set; }
+        /// <summary>
+        /// The start date of the temporary user role.
+        /// </summary>
+        /// <value></value>
+        public DateTime StartDate { get; set; }
+        /// <summary>
+        /// The end date of the temporary user role.
+        /// </summary>
+        /// <value></value>
+        public DateTime ExpirationDate { get; set; }
+    }
+}
