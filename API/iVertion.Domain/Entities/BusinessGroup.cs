3c8268b3cd668dc6203d9a0bc0e6988a835bd3cc
eduardorspada <dudryspada@gmail.com>
2024-04-01 10:08:39 -0300

    We create validation for business group properties.

--- API/iVertion.Domain/Entities/BusinessGroup.cs
+++ API/iVertion.Domain/Entities/BusinessGroup.cs
@@ -1,9 +1,23 @@
 
+using iVertion.Domain.Validation;
+
 namespace iVertion.Domain.Entities
 {
     public class BusinessGroup : Entity
     {
         public string? Name { get; private set; }
         public IEnumerable<Company>? Companies { get; set; }
+
+        private void ValidationDomain(string? name)
+        {
+            DomainExceptionValidation.When(String.IsNullOrEmpty(name),
+                                           "Name cannot be null or empty.");
+            DomainExceptionValidation.When(name?.Length < 5,
+                                           "Name must have at least 5 characters.");
+            DomainExceptionValidation.When(name?.Length > 255,
+                                           "Name must have a maximum of 255 characters.");
+
+            Name = name;
+        }
     }
 }
