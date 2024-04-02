138e10fb45173740ea72569e5ab81581a10a6c0c
eduardorspada <dudryspada@gmail.com>
2024-04-02 16:57:27 -0300

    We implement Company/Type Of Activity entity class.

--- API/iVertion.Domain/Entities/CompanyTypeOfActivity.cs
+++ API/iVertion.Domain/Entities/CompanyTypeOfActivity.cs
@@ -1,8 +1,60 @@
 
+using iVertion.Domain.Validation;
+
 namespace iVertion.Domain.Entities
 {
-    public class CompanyTypeOfActivity : Entity
+    public sealed class CompanyTypeOfActivity : Entity
     {
+        public int CompanyId { get; private set; }
+        public int TypeOfActivityId { get; private set; }
+        public bool IsPrimary { get; private set; }
+        public Company? Company { get; set; }
+        public TypeOfActivity? TypeOfActivity { get; set; }
+
+        public CompanyTypeOfActivity(int companyId,
+                                    int typeOfActivityId,
+                                    bool isPrimary,
+                                    bool active)
+        {
+            ValidationDomain(companyId,
+                            typeOfActivityId);
+            IsPrimary   = isPrimary;
+            Active      = active;
+        }
+        public CompanyTypeOfActivity(int id,
+                                    int companyId,
+                                    int typeOfActivityId,
+                                    bool isPrimary,
+                                    bool active)
+        {
+            DomainExceptionValidation.When(id < 0,
+                                            "Invalid Id, must be up to zero.");
+            ValidationDomain(companyId,
+                            typeOfActivityId);
+            IsPrimary   = isPrimary;
+            Active      = active;
+        }
+        public void Update(int companyId,
+                        int typeOfActivityId,
+                        bool isPrimary,
+                        bool active)
+        {
+            ValidationDomain(companyId,
+                            typeOfActivityId);
+            IsPrimary   = isPrimary;
+            Active      = active;
+        }
+        private void ValidationDomain(int companyId,
+                                    int typeOfActivityId)
+        {
+            DomainExceptionValidation.When(companyId <= 0,
+                                        "Invalid Company Id, must be up to zero.");
+            DomainExceptionValidation.When(typeOfActivityId <= 0,
+                                        "Invalid Type Of Activity Id, must be up to zero.");
+
+            CompanyId           = companyId;
+            TypeOfActivityId    = typeOfActivityId;
         
+        }
     }
 }
