d1e61624f13d392d33b5702a8317509b740ba99e
eduardorspada <dudryspada@gmail.com>
2024-04-02 09:39:43 -0300

    We implement Type Of Geographic Coverage entity class.

--- API/iVertion.Domain/Entities/TypeOfGeographicCoverage.cs
+++ API/iVertion.Domain/Entities/TypeOfGeographicCoverage.cs
@@ -1,8 +1,47 @@
 
+using iVertion.Domain.Validation;
+
 namespace iVertion.Domain.Entities
 {
-    public class TypeOfGeographicCoverage : Entity
+    public sealed class TypeOfGeographicCoverage : Entity
     {
-        
+        public string? Name { get; private set; }
+        public IEnumerable<BusinessGroup>? BusinessGroups { get; set; }
+        public TypeOfGeographicCoverage(string name,
+                                        bool active)
+        {
+            ValidationDomain(name);
+            Active = active;
+
+        }
+        public TypeOfGeographicCoverage(int id,
+                                        string name,
+                                        bool active)
+        {
+            DomainExceptionValidation.When(id <= 0,
+                                           "Invalid Id, must be up to zero.");
+            ValidationDomain(name);
+            Active = active;
+
+        }
+        public void Update(string name,
+                           bool active)
+        {
+            ValidationDomain(name);
+            Active = active;
+
+        }
+
+        private void ValidationDomain(string? name)
+        {
+            DomainExceptionValidation.When(String.IsNullOrEmpty(name),
+                                           "Name cannot be null or empty.");
+            DomainExceptionValidation.When(name?.Length < 5,
+                                           "Name must have at least 5 characters.");
+            DomainExceptionValidation.When(name?.Length > 255,
+                                           "Name must have a maximum of 255 characters.");
+
+            Name = name;
+        } 
     }
 }
