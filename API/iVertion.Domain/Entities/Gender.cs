e8d9b8a2378c1a6d369a77211b50414869823c6b
eduardorspada <dudryspada@gmail.com>
2024-04-16 21:26:35 -0300

    We update Gender entity class.

--- API/iVertion.Domain/Entities/Gender.cs
+++ API/iVertion.Domain/Entities/Gender.cs
@@ -6,50 +6,51 @@ namespace iVertion.Domain.Entities
     public sealed class Gender : Entity
     {
         public string? Description { get; private set; }
-        public char Acronyn { get; private set; }
+        public char Acronym { get; private set; }
+        public IEnumerable<Employee>? Employees { get; set; }
 
         public Gender(string description,
-                      char acronyn,
+                      char acronym,
                       bool active)
         {
             ValidationDomain(description,
-                             acronyn);
+                             acronym);
             Active  = active;
         }
         public Gender(int id,
                       string description,
-                      char acronyn,
+                      char acronym,
                       bool active)
         {
             DomainExceptionValidation.When(id < 0,
                                            "Invalid Id, must be up to zero.");
             ValidationDomain(description,
-                             acronyn);
+                             acronym);
             Id      = id;
             Active  = active;
         }
         public void Update(string description,
-                           char acronyn,
+                           char acronym,
                            bool active)
         {
             ValidationDomain(description,
-                             acronyn);
+                             acronym);
             Active  = active;
         }
 
         private void ValidationDomain(string description,
-                                      char acronyn)
+                                      char acronym)
         {
             DomainExceptionValidation.When(String.IsNullOrEmpty(description),
                                            "Invalid Description, must not be empty or null.");
             DomainExceptionValidation.When(description.Length < 4,
                                            "Invalid Description, too short, minimum 4 characters.");
             DomainExceptionValidation.When(description.Length > 25,
-                                           "Invalid Title, too long, max 25 characters.");
-            DomainExceptionValidation.When(String.IsNullOrEmpty(Char.ToString(acronyn)),
-                                           "Invalid Acronyn, must not be empty or null.");
+                                           "Invalid Description, too long, max 25 characters.");
+            DomainExceptionValidation.When(acronym != 'M' && acronym != 'F',
+                                           "Invalid Acronym, must be 'M' or 'F'.");
             Description = description;
-            Acronyn     = acronyn;
+            Acronym     = acronym;
         }
     }
 }
