e8e783f5466384fe3c7c191ea43f222854750687
eduardorspada <dudryspada@gmail.com>
2024-04-18 21:44:52 -0300

    We implement Remuneration entity class.

--- API/iVertion.Domain/Entities/Remuneration.cs
+++ API/iVertion.Domain/Entities/Remuneration.cs
@@ -1,12 +1,51 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
+using iVertion.Domain.Validation;
 
 namespace iVertion.Domain.Entities
 {
-    public class Remuneration
+    public sealed class Remuneration : Entity
     {
-        
+        public decimal GrossSalary { get; private set; }
+        public int EmployeeId { get; private set; }
+        public Employee? Employee { get; set; }
+        public Remuneration(decimal grossSalary,
+                            int employeId,
+                            bool active)
+        {
+            ValidationDomain(grossSalary,
+                             employeId);
+            Active  = active;
+        }
+        public Remuneration(int id,
+                            decimal grossSalary,
+                            int employeId,
+                            bool active)
+        {
+            DomainExceptionValidation.When(id < 0,
+                                           "Invalid Id, must be up to zero.");
+            ValidationDomain(grossSalary,
+                             employeId);
+            Id      = id;
+            Active  = active;
+        }
+        public void Update(decimal grossSalary,
+                           int employeId,
+                           bool active)
+        {
+            ValidationDomain(grossSalary,
+                             employeId);
+            Active  = active;
+        }
+
+        private void ValidationDomain(decimal grossSalary,
+                                      int employeId)
+        {
+            DomainExceptionValidation.When(grossSalary <= 0,
+                                           "Invalid Gross Salary, must be greater than zero.");
+            DomainExceptionValidation.When(employeId < 0,
+                                           "Invalid Employee ID, must be greater than zero.");
+            GrossSalary = grossSalary;
+            EmployeeId  = employeId;
+        }
     }
 }
