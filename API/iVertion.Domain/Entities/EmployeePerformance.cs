8723a4b0c003c52caecff04ab1fe71918a81dbbb
eduardorspada <dudryspada@gmail.com>
2024-05-14 16:33:14 -0300

    Implemented the class that relates the Employee to Performance classes.

--- API/iVertion.Domain/Entities/EmployeePerformance.cs
+++ API/iVertion.Domain/Entities/EmployeePerformance.cs
@@ -1,8 +1,53 @@
 
+using iVertion.Domain.Validation;
+
 namespace iVertion.Domain.Entities
 {
     public sealed class EmployeePerformance : Entity
     {
-        
+        public int EmployeeId { get; private set; }
+        public IEnumerable<Employee>? Employees { get; set; }
+        public int PerformanceId { get; private set; }
+        public IEnumerable<Performance>? Performances { get; set; }
+
+        public EmployeePerformance(int employeId,
+                                   int performanceId,
+                                   bool active)
+        {
+            ValidationDomain(employeId,
+                             performanceId);
+            Active  = active;
+        }
+        public EmployeePerformance(int id,
+                                   int employeId,
+                                   int performanceId,
+                                   bool active)
+        {
+            DomainExceptionValidation.When(id < 0,
+                                           "Invalid Id, must be up to zero.");
+            ValidationDomain(employeId,
+                             performanceId);
+            Id      = id;
+            Active  = active;
+        }
+        public void Update(int employeId,
+                           int performanceId,
+                           bool active)
+        {
+            ValidationDomain(employeId,
+                             performanceId);
+            Active  = active;
+        }
+
+        private void ValidationDomain(int employeId,
+                                      int performanceId)
+        {
+            DomainExceptionValidation.When(employeId <= 0,
+                                           "Invalid Employee Id, must be up to zero.");
+            DomainExceptionValidation.When(performanceId <= 0,
+                                           "Invalid Performance Id, must be up to zero.");
+            EmployeeId  = employeId;
+            PerformanceId   = performanceId;
+        }
     }
 }
